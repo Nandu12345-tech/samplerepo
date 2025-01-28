08080b61a0d4e96975aa813370333375486f8b39
ASUS <ASUS@LAPTOP-HJLQF8OS>
2025-01-27 21:13:50 -0800

    second commit

---
+++ samplemavel/src/main/java/Aggregation/Aggregation2.java
@@ -0,0 +1,27 @@
+package Aggregation;
+
+public class Aggregation2 {
+	String city;
+	String state;
+	Aggregation1 ref;
+	public Aggregation2(String city,String state, Aggregation1 ref)
+	{
+		this.city =city;
+		this.state=state;
+		this.ref=ref;
+	}
+	public void display()
+	{
+		System.out.println(ref.name+" " +ref.rollno);
+		System.out.println(city+" "+ state);
+	}
+	
+	public static void main(String args[])
+	{
+		Aggregation1 obj =new Aggregation1("anu",20,23);
+		Aggregation2 obj1=new Aggregation2("tvm","kerala",obj);
+		obj1.display();
+	}
+	
+
+}
---
+++ samplemavel/src/main/java/Polymorphism/Polymorphismparent.java
@@ -0,0 +1,19 @@
+package Polymorphism;
+
+public class Polymorphismparent extends Polymorphism1
+{
+	public void display(int a,int b)
+	{
+		int c=a+b;
+		System.out.println("sum of the integer is"+c);
+		super.display(3, 6);
+	}
+	
+	
+	public static void main(String args[])
+{
+		Polymorphismparent obj=new Polymorphismparent();
+		obj.display(4, 2);
+}
+    
+}
